910c6f3b543bf0a740cd8d38eedfec6821fb225c
CarinWood <carin.wood.85@gmail.com>
2024-01-10 18:25:49 +0100

    update

--- index.js
+++ index.js
@@ -82,3 +82,51 @@ function meanValue(values) {
         return sum/values.length
 }
 
+let myArray = [2,4,8,6]
+let myMeanValue = meanValue(myArray)
+console.log('mean value: ' + myMeanValue)
+
+/* Systembolaget */
+
+function checkAge(age) {
+  
+        if(age >= 0 && age <=14) {
+            return "Get out of here!"
+        } 
+        else if (age >= 15 && age <= 17) {
+            return "Sorry, you are not old enough"
+        }
+        else if (age >= 18 && age <= 19) {
+            return "Sorry, you must be atleast 20. Why don't you try a bar instead?"
+        } 
+        else if (age >= 20 && age <= 80) {
+            return "Thank you! Welcome!"
+        }
+        else if(age > 80) {
+            return "I'm sorry, where is your caretaker?"
+        } 
+        else {
+            return "You have to enter a valid age!"
+        }
+
+}
+let message = checkAge(-1)
+console.log(message)
+
+/* Sum */
+
+function calcSum(anArray) {
+    let sum = 0;
+    for (let i = 0; i < anArray.length; i++) {
+        sum += anArray[i]        
+    }
+
+    return sum
+}
+
+const arrayOfTen = [1,2,3,4,5,6,7,8,9,10]
+let finalSum = calcSum(arrayOfTen)
+console.log(finalSum)
+
+/* List a persons' skills */
+
